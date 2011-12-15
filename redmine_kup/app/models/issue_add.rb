db648ad58afc976aa17a10797a7de4164e48300a
Thomas Kohler <gen2thomas@gmx.de>
2011-12-15 18:37:03 +0100

    bugfix: "Bug" can't trace to "Need"

--- redmine_kup/app/models/issue_add.rb
+++ redmine_kup/app/models/issue_add.rb
@@ -12,7 +12,7 @@ class IssueAdd
   KUP_TRACES = { 
     ISSUE_ISBUG   => { :rel_to => ISSUE_ISNEED},
     ISSUE_ISSTORY => { :rel_to => ISSUE_ISNEED},
-    ISSUE_ISNEED  => { :rel_to => [ISSUE_ISSTORY, ISSUE_ISFUNC]},
+    ISSUE_ISNEED  => { :rel_to => [ISSUE_ISBUG, ISSUE_ISSTORY, ISSUE_ISFUNC]},
     ISSUE_ISFUNC  => { :rel_to => [ISSUE_ISNEED, ISSUE_ISFEAT, ISSUE_ISSPEC, ISSUE_ISTESTC]},
     ISSUE_ISFEAT  => { :rel_to => ISSUE_ISFUNC},
     ISSUE_ISSPEC  => { :rel_to => ISSUE_ISFUNC},
