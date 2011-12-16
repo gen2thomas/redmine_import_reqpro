cba997fb00b2139e8b8ca774de0ad31a97761506
Thomas Kohler <gen2thomas@gmx.de>
2011-12-16 21:01:32 +0100

    big renaming rpb --> reqpro

---
+++ test/unit/reqproimport_in_progress_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ReqprimportInProgressTest < ActiveSupport::TestCase
+  fixtures :reqproimport_in_progresses
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
