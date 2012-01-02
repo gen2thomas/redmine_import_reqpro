a61d3c254cd4b7b2b59765d38c553388ef1fc291
Thomas Kohler <gen2thomas@gmx.de>
2012-01-02 19:43:30 +0100

    improved user import

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -65,7 +65,7 @@ module UsersHelper
     return rpusers
   end  
   
-  def get_fullname(fullname_old, email)
+  def get_fullname(fullname_old, email, login)
     # firstname and lastname are in the same string
     fullname = Hash.new
     fullname[:firstname] = nil
@@ -74,15 +74,15 @@ module UsersHelper
     # case 1: space is the delimiter "firstname lastname"
     splitname = fullname_old.split(/\s/)
     if splitname.length == 2
-      fullname[:firstname] = splitname[0]
-      fullname[:lastname] = splitname[1]      
+      fullname[:firstname] = splitname[0].capitalize
+      fullname[:lastname] = splitname[1].capitalize
     end
     # case 2: uppercase letters are the signs "FirstnameLastname" or "Firstname lastname"
     if fullname[:firstname] == nil
       splitname = fullname_old.split(/([A-Z][a-z]*)/)
       if splitname.length == 4
-        fullname[:firstname] = splitname[1]
-        fullname[:lastname] = splitname[3]      
+        fullname[:firstname] = splitname[1].capitalize
+        fullname[:lastname] = splitname[3].capitalize
       end
     end
     # check against errors and use mail
@@ -93,20 +93,23 @@ module UsersHelper
         email1 = email.split(/[@]/) # ["firstname.lastname","gmx.de"]
         splitname = email1[0].split(/([A-Z]?[a-z]*)([.]?)/) # ["", "firstname", "", "", "Lastname"]
         if splitname.length == 5
-          fullname[:firstname] = splitname[1]
-          fullname[:lastname] = splitname[4]      
+          fullname[:firstname] = splitname[1].capitalize
+          fullname[:lastname] = splitname[4].capitalize      
         end 
       end
     end
-    # case 4: find login inside email
-    #TODO: not done yet
+    # case 4: try to find login inside email
     # case 5: firstname@lastname...
     if email1 != nil
       if fullname[:firstname] == nil
-        fullname[:firstname] = email1[0]
-      end
-      if fullname[:lastname] == nil
-        fullname[:lastname] = email1[1]
+        fullname[:firstname] = email1[0].downcase.split(login.downcase)[0].capitalize
+        if fullname[:firstname].downcase != email1[0].downcase
+          #login was found
+          fullname[:lastname] = login.capitalize
+        else
+          #login was not found, use last part of mail
+          fullname[:lastname] = email1[1]
+        end
       end
     end
     # last way out:
@@ -123,7 +126,7 @@ module UsersHelper
   # rpusers[key] ={:firstname => "Firstname", :lastname => "Lastname", :rmuser => rm-user}
   # if a rpuser is found --> check the rmusers for existenz
   def find_user_by_string(rp_user_string, rpusers)
-    rp_fullname = get_fullname(rp_user_string, nil)
+    rp_fullname = get_fullname(rp_user_string, nil, nil)
     found_user = nil
     # best level:
     rpusers.each_value do |a_rpuser|
@@ -171,8 +174,8 @@ module UsersHelper
   
 private
   def collect_users_fast(some_projects, conflate_users)
-    #get an data path to open an ProjectStructure file
-    #collect all prefixes and guids to an array of hash
+    #get an data path to open an ProjectUserGroups file
+    #collect all users to an array of hash
     # add a possible ":conf_key" (conflation-key)
     users = Hash.new
     some_projects.each_value do |a_project|
@@ -183,13 +186,13 @@ private
           hash_key = user.attributes["ID"]
           if users[hash_key] == nil #not known
             users[hash_key] = Hash.new
-            users[hash_key] [:project] = a_project[:prefix] 
+            users[hash_key] [:project] = a_project[:prefix]
+            users[hash_key] [:email] = user.attributes["EmailAddress"] 
             users[hash_key] [:login] = user.attributes["LoginName"]
-            fullname = get_fullname(user.attributes["FullName"],user.attributes["EmailAddress"])
+            fullname = get_fullname(user.attributes["FullName"], users[hash_key] [:email], users[hash_key] [:login])
             users[hash_key] [:firstname] = fullname[:firstname]
             users[hash_key] [:lastname] = fullname[:lastname]
             users[hash_key] [:group] = user.attributes["GroupGUID"]
-            users[hash_key] [:email] = user.attributes["EmailAddress"]
           else
             #this should never be the case --> better is to raise an error in that case 
             #make an array and add the next project
