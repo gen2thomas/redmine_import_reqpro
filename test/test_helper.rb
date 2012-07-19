cd4c0bb69543de1c32db53d752c34770a03c6703
Thomas Kohler <gen2thomas@gmx.de>
2012-07-19 21:17:32 +0100

    unit tests added

---
+++ test/test_helper.rb
@@ -0,0 +1,16 @@
+# Load the normal Rails helper
+require File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper')
+
+#require 'rubygems'
+#require 'test/unit'
+#require 'multi_rails_init'
+# gem 'activerecord', '>= 2.0'
+#require 'active_record' 
+#require 'action_controller'
+#require 'action_view'
+#require 'active_record/fixtures'
+
+
+
+# Ensure that we are using the temporary fixture path
+#Engines::Testing.set_fixture_path
